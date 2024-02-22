91ecfed15b62e0e9da2bf52da41f626d548a5657
Johannes Lakkisto <johannes.lakkisto@ssh.com>
2024-02-22 13:42:50 +0200

    centos 7 compatible random source

--- src/system/linux/system/urandom.hpp
+++ src/system/linux/system/urandom.hpp
@@ -11,6 +11,8 @@ SPDX-License-Identifier: GPL-2.0-or-later
 #include "utils/random.hpp"
 
 #include <limits>
+
+#ifndef COMPAT_CENTOS7
 #include <sys/random.h>
 
 
@@ -43,3 +45,64 @@ public:
         }
     }
 };
+#else // COMPAT_CENTOS7
+#include <cstdio>
+
+const size_t RAND_BUF_SIZE = 1024;
+
+class URandom final : public Random
+{
+public:
+    URandom() {
+        std::FILE* f = std::fopen("/dev/urandom", "r");
+        if (!f) {
+            LOG(LOG_ERR, "failed to open /dev/urandom");
+            throw Error(ERR_RANDOM_SOURCE_FAILED);
+        }
+        LOG(LOG_INFO, "opened /dev/urandom");
+        _devRandom = f;
+        _bufSize = 0;
+    }
+
+    ~URandom() {
+        LOG(LOG_INFO, "closed /dev/urandom");
+        std::fclose(_devRandom);
+    }
+
+    void random(writable_bytes_view buf) override {
+        uint8_t* data = buf.data();
+        size_t len = buf.size();
+        size_t written = 0;
+
+        while (written < len) {
+            if (_bufSize == 0) {
+                size_t n = fread(_buf, sizeof(uint8_t), RAND_BUF_SIZE, _devRandom);
+                if (n != RAND_BUF_SIZE) {
+                    LOG(LOG_ERR, "random source failed to provide random data [%s]", strerror(errno));
+                    throw Error(ERR_RANDOM_SOURCE_FAILED);
+                }
+                LOG(LOG_INFO, "read from /dev/urandom");
+                _bufSize = RAND_BUF_SIZE;
+            }
+            size_t n = len - written;
+            if (len > _bufSize) {
+                n = _bufSize;
+            }
+            memcpy(data + written, _buf + (RAND_BUF_SIZE - _bufSize), n);
+            _bufSize -= n;
+            written += n;
+        }
+        LOG(LOG_INFO, "returned %d bytes of random data", len);
+        // size_t n = fread(data, sizeof(uint8_t), len, _devRandom);
+        // if (n != len) {
+        //     LOG(LOG_ERR, "random source failed to provide random data [%s]", strerror(errno));
+        //     throw Error(ERR_RANDOM_SOURCE_FAILED);
+        // }
+    }
+
+protected:
+    std::FILE* _devRandom;
+    uint8_t _buf[RAND_BUF_SIZE];
+    size_t _bufSize;
+};
+#endif // COMPAT_CENTOS7
